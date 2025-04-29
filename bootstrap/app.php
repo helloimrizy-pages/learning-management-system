6ea1df547abf9d4710d706cf49e6644d735601c6
Alfarizy <rizy.izy15@gmail.com>
2025-04-29 18:28:50 +0200

    Add middleware aliases for authentication and role verification in app configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'auth' => \App\Http\Middleware\Authenticate::class,
+            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+            'teacher' => \App\Http\Middleware\TeacherMiddleware::class,
+            'student' => \App\Http\Middleware\StudentMiddleware::class,
+        ]);        
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
