e22329a50385a2cfaef5c74ad3b86d52a5ed9a01
Alfarizy <rizy.izy15@gmail.com>
2025-04-29 18:04:18 +0200

    Add Subject model with relationships to User and Task

---
+++ app/Models/Subject.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Subject extends Model
+{
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function students()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+}
