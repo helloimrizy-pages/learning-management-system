b9a7267ffe909ebf8ac1b9036625100765fdfd31
Alfarizy <rizy.izy15@gmail.com>
2025-04-29 22:13:40 +0200

    Refactor Subject model to include HasFactory trait and define fillable properties

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Subject extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'code',
+        'credit',
+    ];
+    
     public function teacher()
     {
         return $this->belongsTo(User::class, 'user_id');
