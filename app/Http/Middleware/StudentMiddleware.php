3207c7aa404503b59ebfc9972fac4b767457d8ec
Alfarizy <rizy.izy15@gmail.com>
2025-04-29 18:28:05 +0200

    Refactor StudentMiddleware to enforce student role check before request handling

--- app/Http/Middleware/StudentMiddleware.php
+++ app/Http/Middleware/StudentMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class StudentMiddleware
 {
@@ -13,8 +14,11 @@ class StudentMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next)
     {
-        return $next($request);
+        if (Auth::check() && Auth::user()->role == 'student') {
+            return $next($request);
+        }
+        abort(403);
     }
 }
