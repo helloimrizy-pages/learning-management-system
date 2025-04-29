c480b8cd6412a7df1ad4002a7ac57c4b44ecd5fb
Alfarizy <rizy.izy15@gmail.com>
2025-04-29 22:13:48 +0200

    Refactor Task model to include HasFactory trait and define fillable properties

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'points',
+        'subject_id',
+    ];
+    
     public function subject()
     {
         return $this->belongsTo(Subject::class);
